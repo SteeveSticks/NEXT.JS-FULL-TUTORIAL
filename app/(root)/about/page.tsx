f3fd84086cd058cc50b0217f7c04e1ba47ac5a53
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-04-17 01:23:14 +0100

    data fetching

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const page = () => {
-  return <div>About</div>;
-};
+async function Home() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
 
-export default page;
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  const albums = await response.json();
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-col-2 md:grid-cols">
+      {albums.map((album: { id: number; title: string }) => (
+        <div
+          key={album.id}
+          className="bg-white shadow-md rounded-lg p-4 transition"
+        >
+          <h3 className="text-lg font-bold mb-2">{album.title}</h3>
+          <p className="text-lg font-bold mb-2">{album.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
